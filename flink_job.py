471231772470382ad56e94f0cab7d4263a7e4ac2
David Sachs <sachs.davidr@gmail.com>
2025-06-20 20:19:09 -0400

    add flink job code and comments

--- flink_job.py
+++ flink_job.py
@@ -4,6 +4,12 @@ from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.common.typeinfo import Types
 from pyflink.datastream.connectors import FlinkKafkaConsumer
 
+#make the execution environment for our dataflow pipeline
+#execution environment is a container, sort of. 
+#it holds the configuration
+#it holds what we define below...registers them in the "DAG builder"(?) behind the scenes
+#eventually, we'll call execute on it
+
 env = StreamExecutionEnvironment.get_execution_environment()
 
 # Define Kafka consumer
@@ -19,13 +25,24 @@ consumer = FlinkKafkaConsumer(
     properties=kafka_props
 )
 
+#ds stands for data steam.  we are defining the data stream and we'll execute later
+# start by adding the source (kafka consumer)
 ds = env.add_source(consumer)
 
-# Convert raw JSON strings to Python dicts
+# 
+# for each item in this data stream, apply this function:
+# - (lambda raw: json.loads(raw))
+#   - i.e. convert raw JSON strings to Python dicts
+# this is a transformation...and it is now part of the DAG (directed acyclic graph) that...
+# ...Flink will execute when env.execute() is called.
+# "parsed" is a "DataStream object"...its an edge of the DAG
 parsed = ds.map(lambda x: json.loads(x), output_type=Types.MAP(Types.STRING(), Types.STRING()))
 
 # Print the output (to stdout for now)
+# this is a "sink"
 parsed.print()
 
 # Run the job
+# (serialize the job?)
+# It will be called "Flink Kafka Log Consumer" in 
 env.execute("Flink Kafka Log Consumer")
--- python-learning-tidbits.md
+++ python-learning-tidbits.md
@@ -1,12 +1,15 @@
 run a line of code from the command prompt (bash)
 - python3 -c "insert code here"
+
 run a module as if it were a script..., via command prompt (bash)...
 - useful, especially, for ensuring the module runs via the right python executable (interpreter)?
 - python -m modulename modulecommand
   - example: python3 -m pip install kafka-python
     - running it this way, vs pip install kafka-python, ensures pip installs the code to that specific interpreter's library (vs another?)?
+
 virtual environment stuff
-- virtual env environment...and interpreter (that lives in virtual env path) are what are used when in virtual environment....via adding it to $PATH variable?
+- virtual env environment...and interpreter (that lives in virtual env path) are what are used when in 
+virtual environment....via adding it to $PATH variable?
 - create a virtual environment
   - python3 -m venv .venv
 - activate a virtual environment
@@ -18,4 +21,8 @@ virtual environment stuff
   - pip freeze > requirements.txt
 - recreate your venv (dependencies)
   - pip install -r requirements.txt
+- understand your virtual environment...which python interpreter you are using, where pip points, what pip installed
+  - which python
+  - which pip
+  - python -m pip list
  
